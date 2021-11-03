0c372bed6fe38cfe1fb57a118596c88110153c97
William Terrill <william.terrill@gmail.com>
2021-11-03 12:43:39 -0600

    updated UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
-import 'dart:html';
-import 'dart:ui' as ui;
-import 'dart:js' as js;
+// import 'dart:html';
+// import 'dart:ui' as ui;
+// import 'dart:js' as js;
 
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
--- lib/post_page.dart
+++ lib/post_page.dart
@@ -5,6 +5,7 @@ import 'dart:convert';
 
 import 'package:tmtfinancialconnect/financial_dashboard.dart';
 
+import 'colorDefs.dart';
 import 'http_api.dart';
 
 BuildStructure? finalStructure;
@@ -20,7 +21,7 @@ class PostPage extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(
-        title: Text('post page'),
+        title: Text('Quickbooks Configuration'),
       ),
       body: Center(
         child: Column(
@@ -28,19 +29,32 @@ class PostPage extends StatelessWidget {
           children: <Widget>[
             FinancialDashboard(),
             TextButton(
-                onPressed: () {
-                  print('pressed');
-                  HttpApi http = HttpApi();
-                  dynamic beer = http.postData('title');
-                  beer.then((Response val) {
-                    print('%%%%%%%%%%%%-------');
-                    print('response from server received');
-                    print(val.body);
-                    print('%%%%%%%%%%%%-------');
-                  });
-                },
-                child: Text('Submit')),
-            SelectableText(json.decode(data).toString()),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text('Submit'),
+              ),
+              style: TextButton.styleFrom(
+                primary: Colors.white,
+                backgroundColor: ColorDefs.buttonBackground,
+                shadowColor: Colors.black,
+                elevation: 5,
+                side: BorderSide(color: Colors.red, width: 1),
+                textStyle: const TextStyle(fontSize: 20),
+              ),
+              onPressed: () {
+                print('pressed');
+                HttpApi http = HttpApi();
+                dynamic beer = http.postData('title');
+                beer.then((Response val) {
+                  print('%%%%%%%%%%%%-------');
+                  print('response from server received');
+                  print(val.body);
+                  print('%%%%%%%%%%%%-------');
+                });
+              },
+            ),
+
+            // SelectableText(json.decode(data).toString()),
           ],
         ),
       ),
